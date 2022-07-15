552810c4bd0a49701d2496f56e7e505cd646431d
ousainoujaiteh <ousainoujaiteh@gmail.com>
2022-07-15 10:38:56 +0000

    Initial Commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '729003c2b1msh16bb03a11c160ebp13c7bejsnf9b49d58240e',
+		'X-RapidAPI-Key': '', // enter your rapid api key here
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "a9879597ada1d6c97cbcd88ee5e6ab40";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
